f85a000394d7c3144c00eaf50788bbd8e22f0286
20DianaRivero01 <dianarivero0120@gmail.com>
2020-06-15 22:58:27 -0500

    first commit

--- Ex13_1.c
+++ Ex13_1.c
@@ -7,21 +7,17 @@
 
 int main(){
 
-  int numbers[NUM] = {1,2,3,4,5,6,7,8,9,10,11}; // Variable array of numbers
+	int numbers[NUM] = {1,2,3,4,5,6,7,8,9,10,11}; // Variable array of numbers
+	int *n_num; // Pointer Var
+	int n;
 
-  int *n_num; // Pointer Var
+  		for(n_num = &numbers[0];n_num < &numbers[NUM];n_num++){ // A bucle for to check if my var reach the total size of the array (11).
+		*n_num = 0; // It wil make the output print for each var = 0
+		}
 
-  int n;
+			for(n=0;n<NUM;n++){
+			printf("The number [%i] is: %i\n",n,numbers[n]); //the numbers will start counting from 0 to 10
+  			}
 
-  for(n_num = &numbers[0];n_num < &numbers[NUM];n_num++){ // A bucle for to check if my var reach the total size of the array (11).
-
-    *n_num = 0; // It wil make the output print for each var = 0
-  }
-
-  for(n=0;n<NUM;n++){
-
-    printf("The number [%i] is: %i\n",n,numbers[n]); //the numbers will start counting from 0 to 10
-  }
-
-  return 0;
+return 0;
 }
